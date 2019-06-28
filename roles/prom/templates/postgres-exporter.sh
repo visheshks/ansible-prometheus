e55610b867a4c2ee8895726ac5c03437414db93b
Vishesh Kumar Singh <kuldeep.saini@innovaccer.com>
2019-06-28 18:22:31 +0530

    adding postgres

---
+++ roles/prom/templates/postgres-exporter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DATA_SOURCE_NAME="postgresql://inno_dupage:Innovation123@dupage-inapi.cluster-c939ocue91rj.us-east-1.rds.amazonaws.com:5432/inapi?sslmode=disable" /usr/local/bin/postgres-exporter
+
